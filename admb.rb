42e6f3b3e0a633d21f684d68d2f5f4fe7d46b2ac
Jonathan Terhorst <terhorst@gmail.com>
2014-05-01 17:37:24 -0700

    twake

--- admb.rb
+++ admb.rb
@@ -1,16 +1,9 @@
 require "formula"
 
-# Documentation: https://github.com/Homebrew/homebrew/wiki/Formula-Cookbook
-#                /usr/local/Library/Contributions/example-formula.rb
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Admb < Formula
   homepage "http://www.admb-project.org"
   url "http://admb-project.googlecode.com/files/admb-11.1.zip"
   sha1 "d728712cbf9b123e5339b9e77f806c7c49cc3086"
-
-  # depends_on "cmake" => :build
-  # depends_on :x11 # if your formula requires any X11/XQuartz components
 
   def install
     ENV.deparallelize  # if your formula fails when building in parallel
@@ -20,19 +13,7 @@ class Admb < Formula
     end
 
     system "make" # if this fails, try separate make/make install steps
-    system "mv build/dist #{prefix}"
-  end
-
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test admb`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "mv build/dist/* #{prefix}"
   end
 
   def caveats
